eecd0483ebb4fa9845d8e7bcf14a895fc9abd888
unknown <chronobait@gmail.com>
2024-02-25 13:55:45 +0800

    Added ReverseString

---
+++ ReverseString/ReverseString.py
@@ -0,0 +1,6 @@
+def reverse_string(input_string):
+    return input_string[::-1]
+
+input_string = input("Enter string to reverse: ")
+reversed_string = reverse_string(input_string)
+print(reversed_string)
