bdc36385d257a46fa79cfc787ce80f4a6e68a401
unknown <chronobait@gmail.com>
2024-02-25 13:12:55 +0800

    Added New Files

---
+++ Factorial/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool error = true;
+            do
+            {
+                try
+                {
+                    Console.Write("Enter non-negative number: ");
+                    int number = Convert.ToInt32(Console.ReadLine());
+
+                    if (number >= 0)
+                        Console.WriteLine($"Factorial of {number} is: {GetFactorial(number)}");
+                    else
+                        throw new ArithmeticException();
+
+                    error = false;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("INVALID ENTRY!");
+                }
+            } while (error == true);
+
+        }
+
+        static long GetFactorial(int number)
+        {
+            if (number == 0)
+                return 1;
+            else
+                return number * GetFactorial(number - 1);
+        }
+    }
+}
